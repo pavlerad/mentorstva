c6e99b5f595d8ac4709edb733675133763f2b13d
djilkosh <pavle11radovanovic@gmail.com>
2025-05-06 21:45:25 +0200

    renamed files

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+
+
+with open ("data.json", "r") as file:
+    data = json.load(file)
+    print(data)
+    data.append({"name": "Dijana",
+                    "age": 23,
+                  "gender": "female",
+                  "height": 187})
+    print(data)
+
+with open ("data.json", "w") as file:
+    json.dump(data, file, indent = 5)
+    print(data)
