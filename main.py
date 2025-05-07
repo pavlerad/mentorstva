4910f13b6c0f346c63e3819fdee9ccdc471069ef
djilkosh <pavle11radovanovic@gmail.com>
2025-05-07 09:09:45 +0200

    created data.json

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 import json
 
 
-from methods import load_file
-
-
-products = load_file("data/products.json")
-print(products)
-
-
-
-with open ("data/user.json", "r") as file:
+with open ("data.json", "r") as file:
     data = json.load(file)
     print(data)
-    data.append({"name": "Dijana",
-                    "age": 23,
-                  "gender": "female",
-                  "height": 187})
-    print(data)
-
-with open ("data/user.json", "w") as file:
-    json.dump(data, file, indent = 5)
-    print(data)
-
-
-from methods import load_file
