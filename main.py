47cca129846a3dc7115fef64684e00e6e901fb82
djilkosh <pavle11radovanovic@gmail.com>
2025-05-07 09:25:37 +0200

    created data.json, renamed it to "user.json", created "products.json", created a folder named "data"

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import json
 
 
-with open ("data.json", "r") as file:
+with open ("data/user.json", "r") as file:
     data = json.load(file)
+    data.append({     "name" : "Milos",
+                      "age" : 24,
+                      "height": 160,
+                      "gender": "male"})
     print(data)
+
+
+    with open("data/user.json", "w") as file:
+        json.dump(data, file, indent = 5)
+
+print(data)
+
+
+# r = read,
+# w = write
+
